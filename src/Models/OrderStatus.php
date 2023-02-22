dba2ebd44e112df5ba17946c882be55c5ea056a3
FusionPwN <fusionpwnu@gmail.com>
2023-02-22 16:22:20 +0000

    Update OrderStatus.php

--- src/Models/OrderStatus.php
+++ src/Models/OrderStatus.php
@@ -64,7 +64,7 @@ class OrderStatus extends Enum implements OrderStatusContract
 	protected static $closedStatuses = [self::CANCELLED, self::COMPLETED];
 	protected static $paidStatuses = [self::PAID, self::DISPATCHED, self::ON_BILLING, self::COMPLETED];
 	protected static $stockStatuses = [self::IN_CREATION, self::PENDING, self::PAID, self::ON_BILLING];
-	protected static $editableStatuses = [self::IN_CREATION, self::PENDING];
+	protected static $editableStatuses = [self::IN_CREATION];
 
 	# para a app
 	protected static $statusColors = [
