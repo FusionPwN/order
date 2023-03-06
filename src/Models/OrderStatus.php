43ae190f90a4af80816ba7559f2ef4b55fcac00b
FusionPwN <fusionpwnu@gmail.com>
2023-03-06 12:40:33 +0000

    Update OrderStatus.php

--- src/Models/OrderStatus.php
+++ src/Models/OrderStatus.php
@@ -18,69 +18,46 @@ use Vanilo\Order\Contracts\OrderStatus as OrderStatusContract;
 
 class OrderStatus extends Enum implements OrderStatusContract
 {
-	const __DEFAULT = self::PENDING;
-
-	const IN_CREATION = 'em_criacao';
-
-	/**
-	 * Pending orders are brand new orders that have not been processed yet.
-	 */
-	const PENDING = 'pendente';
-
-	/**
-	 * Payed orders are brand new orders that have not been processed yet.
-	 */
-	const PAID = 'paga';
-
-	/**
-	 * Delivered orders are brand new orders that have not been processed yet.
-	 */
-	const DISPATCHED = 'expedida';
-
-	/**
-	 * Delivered orders are brand new orders that have not been processed yet.
-	 */
-	const ON_BILLING = 'em_faturacao';
-
-	/**
-	 * Delivered orders are brand new orders that have not been processed yet.
-	 */
-	const BILLED = 'faturada';
-
-	/**
-	 * Orders fulfilled completely.
-	 */
-	const COMPLETED = 'concluida';
-
-	/**
-	 * Order that has been cancelled.
-	 */
-	const CANCELLED = 'cancelada';
+	const __DEFAULT 			= self::PENDING;
+	const IN_CREATION 			= 'em_criacao';
+	const PENDING 				= 'pendente';
+	const PAID 					= 'paga';
+	const DISPATCHED 			= 'expedida';
+	const ON_BILLING 			= 'em_faturacao';
+	const BILLED 				= 'faturada';
+	const COMPLETED 			= 'concluida';
+	const CANCELLED 			= 'cancelada';
+	const AWAITS_CONFIRMATION 	= 'aguarda_confirmacao';
+	const AWAITS_PAYMENT 		= 'aguarda_pagamento';
 
 	// $labels static property needs to be defined
 	public static $labels = [];
 
-	protected static $openStatuses = [self::IN_CREATION, self::PENDING, self::PAID, self::DISPATCHED, self::ON_BILLING];
-	protected static $closedStatuses = [self::CANCELLED, self::COMPLETED];
-	protected static $paidStatuses = [self::PAID, self::DISPATCHED, self::ON_BILLING, self::COMPLETED];
-	protected static $stockStatuses = [self::IN_CREATION, self::PENDING, self::PAID, self::ON_BILLING];
-	protected static $editableStatuses = [self::IN_CREATION];
+	protected static $openStatuses 			= [self::IN_CREATION, self::PENDING, self::PAID, self::DISPATCHED, self::ON_BILLING];
+	protected static $closedStatuses 		= [self::CANCELLED, self::COMPLETED];
+	protected static $paidStatuses 			= [self::PAID, self::DISPATCHED, self::ON_BILLING, self::COMPLETED];
+	protected static $stockStatuses 		= [self::IN_CREATION, self::PENDING, self::PAID, self::ON_BILLING];
+	protected static $editableStatuses 		= [self::IN_CREATION];
 
 	# para a app
 	protected static $statusColors = [
-		self::PENDING 		=> '#de972d',
-		self::PAID			=> '#3399d8',
-		self::CANCELLED 	=> '#dd302a',
-		self::COMPLETED 	=> '#349ed2',
-		self::DISPATCHED 	=> '#85c62c',
+		self::PENDING 				=> '#de972d',
+		self::PAID					=> '#3399d8',
+		self::CANCELLED 			=> '#dd302a',
+		self::COMPLETED 			=> '#349ed2',
+		self::DISPATCHED 			=> '#85c62c',
+		self::AWAITS_CONFIRMATION 	=> '#FF6700',
+		self::AWAITS_PAYMENT 		=> '#FF6700',
 	];
 
 	protected static $statusIcons = [
-		self::PENDING 		=> 'fa.clock',
-		self::PAID			=> 'fa.moneyBillAlt',
-		self::CANCELLED 	=> 'fa.ban',
-		self::COMPLETED 	=> 'fa.calendarCheck',
-		self::DISPATCHED 	=> 'fa.shippingFast',
+		self::PENDING 				=> 'fa.clock',
+		self::PAID					=> 'fa.moneyBillAlt',
+		self::CANCELLED 			=> 'fa.ban',
+		self::COMPLETED 			=> 'fa.calendarCheck',
+		self::DISPATCHED 			=> 'fa.shippingFast',
+		self::AWAITS_CONFIRMATION 	=> 'fa.exclamationCircle',
+		self::AWAITS_PAYMENT 		=> 'fa.exclamationCircle',
 	];
 
 	public function isOpen(): bool
@@ -141,14 +118,16 @@ class OrderStatus extends Enum implements OrderStatusContract
 	protected static function boot()
 	{
 		static::$labels = [
-			self::IN_CREATION => __('backoffice.order.in_creation'),
-			self::PENDING     => __('backoffice.order.pending'),
-			self::COMPLETED   => __('backoffice.order.completed'),
-			self::CANCELLED   => __('backoffice.order.cancelled'),
-			self::PAID        => __('backoffice.order.paid'),
-			self::DISPATCHED  => __('backoffice.order.dispatched'),
-			self::ON_BILLING  => __('backoffice.order.on_billing'),
-			self::BILLED      => __('backoffice.order.billed'),
+			self::IN_CREATION 			=> __('backoffice.order.in_creation'),
+			self::PENDING     			=> __('backoffice.order.pending'),
+			self::COMPLETED   			=> __('backoffice.order.completed'),
+			self::CANCELLED   			=> __('backoffice.order.cancelled'),
+			self::PAID        			=> __('backoffice.order.paid'),
+			self::DISPATCHED  			=> __('backoffice.order.dispatched'),
+			self::ON_BILLING  			=> __('backoffice.order.on_billing'),
+			self::BILLED      			=> __('backoffice.order.billed'),
+			self::AWAITS_CONFIRMATION 	=> __('backoffice.order.awaits-confirmation'),
+			self::AWAITS_PAYMENT 		=> __('backoffice.order.awaits-payment'),
 		];
 	}
 }
