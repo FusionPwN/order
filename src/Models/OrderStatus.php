af2f3987d7ebac5b41ba4ed0518c7ae9fb94557f
QuackZs <marco_marconis@hotmail.com>
2023-10-31 10:21:34 +0000

    update

--- src/Models/OrderStatus.php
+++ src/Models/OrderStatus.php
@@ -19,41 +19,45 @@ use Illuminate\Support\Facades\Cache;
 
 class OrderStatus extends Enum implements OrderStatusContract
 {
-	const __DEFAULT 				= self::PENDING;
-	const IN_CREATION 				= 'em_criacao';
-	const AWAITS_CONFIRMATION 		= 'aguarda_confirmacao';
-	const AWAITS_PAYMENT 			= 'aguarda_pagamento';
-	const PENDING 					= 'pendente';
-	const PAID 						= 'paga';
-	const IN_WAREHOUSE_PREPARATION 	= 'em_preparacao_armazem';
-	const PROCESSING 				= 'em_processamento';
-	const ON_BILLING 				= 'em_faturacao';
-	const BILLED 					= 'faturada';
-	const DISPATCHED 				= 'expedida';
-	const COMPLETED 				= 'concluida';
-	const REFUNDING 				= 'em_devolucao';
-	const REFUNDED 					= 'devolvido';
-	const CANCELLED 				= 'cancelada';
+	const __DEFAULT 					= self::PENDING;
+	const IN_CREATION 						= 'em_criacao';
+	const AWAITS_CONFIRMATION 				= 'aguarda_confirmacao';
+	const AWAITS_PAYMENT 					= 'aguarda_pagamento';
+	const PENDING 							= 'pendente';
+	const PAID 								= 'paga';
+	const IN_WAREHOUSE_PREPARATION 			= 'em_preparacao_armazem';
+	const IN_PREPARATION_PHARMACY_STORE 	= 'em_preparacao_farmacia_loja';
+	const PROCESSING 						= 'em_processamento';
+	const ON_BILLING 						= 'em_faturacao';
+	const BILLED 							= 'faturada';
+	const DISPATCHED 						= 'expedida';
+	const READY_FOR_PICKUP					= 'pronta_para_levantamento';
+	const COMPLETED 						= 'concluida';
+	const REFUNDING 						= 'em_devolucao';
+	const REFUNDED 							= 'devolvido';
+	const CANCELLED 						= 'cancelada';
 	
 
 	// $labels static property needs to be defined
 	public static $labels = [];
 
 	protected static $visibility = [
-		self::IN_CREATION				=> true,
-		self::AWAITS_CONFIRMATION 		=> true,
-		self::AWAITS_PAYMENT 			=> true,
-		self::PENDING 					=> true,
-		self::PAID						=> true,
-		self::CANCELLED 				=> true,
-		self::COMPLETED 				=> true,
-		self::DISPATCHED 				=> true,
-		self::ON_BILLING 				=> true,
-		self::BILLED 					=> true,
-		self::IN_WAREHOUSE_PREPARATION 	=> true,
-		self::PROCESSING 				=> true,
-		self::REFUNDING 				=> true,
-		self::REFUNDED 					=> true,
+		self::IN_CREATION						=> true,
+		self::AWAITS_CONFIRMATION 				=> true,
+		self::AWAITS_PAYMENT 					=> true,
+		self::PENDING 							=> true,
+		self::PAID								=> true,
+		self::CANCELLED 						=> true,
+		self::COMPLETED 						=> true,
+		self::DISPATCHED 						=> true,
+		self::ON_BILLING 						=> true,
+		self::BILLED 							=> true,
+		self::IN_WAREHOUSE_PREPARATION 			=> true,
+		self::IN_PREPARATION_PHARMACY_STORE 	=> true,
+		self::READY_FOR_PICKUP 					=> true,
+		self::PROCESSING 						=> true,
+		self::REFUNDING 						=> true,
+		self::REFUNDED 							=> true,
 	];
 
 	public static function choices()
@@ -70,10 +74,10 @@ class OrderStatus extends Enum implements OrderStatusContract
 		return $result;
 	}
 
-	protected static $openStatuses 			= [self::IN_CREATION, self::AWAITS_CONFIRMATION, self::PENDING, self::AWAITS_PAYMENT, self::PAID, self::DISPATCHED, self::ON_BILLING, self::IN_WAREHOUSE_PREPARATION, self::PROCESSING,self::REFUNDING];
+	protected static $openStatuses 			= [self::IN_CREATION, self::AWAITS_CONFIRMATION, self::PENDING, self::AWAITS_PAYMENT, self::PAID, self::DISPATCHED, self::ON_BILLING, self::IN_WAREHOUSE_PREPARATION, self::PROCESSING,self::REFUNDING,self::IN_PREPARATION_PHARMACY_STORE,self::READY_FOR_PICKUP];
 	protected static $closedStatuses 		= [self::CANCELLED, self::COMPLETED, self::REFUNDED ];
-	protected static $paidStatuses 			= [self::PAID, self::DISPATCHED, self::ON_BILLING, self::BILLED, self::COMPLETED, self::IN_WAREHOUSE_PREPARATION, self::PROCESSING,self::REFUNDING];
-	protected static $stockStatuses 		= [self::IN_CREATION, self::AWAITS_CONFIRMATION, self::PENDING, self::AWAITS_PAYMENT, self::PAID, self::DISPATCHED, self::ON_BILLING, self::IN_WAREHOUSE_PREPARATION, self::PROCESSING];
+	protected static $paidStatuses 			= [self::PAID, self::DISPATCHED, self::ON_BILLING, self::BILLED, self::COMPLETED, self::IN_WAREHOUSE_PREPARATION, self::PROCESSING,self::REFUNDING,self::IN_PREPARATION_PHARMACY_STORE,self::READY_FOR_PICKUP];
+	protected static $stockStatuses 		= [self::IN_CREATION, self::AWAITS_CONFIRMATION, self::PENDING, self::AWAITS_PAYMENT, self::PAID, self::DISPATCHED, self::ON_BILLING, self::IN_WAREHOUSE_PREPARATION, self::PROCESSING,self::IN_PREPARATION_PHARMACY_STORE ];
 	protected static $editableStatuses 		= [self::IN_CREATION, self::AWAITS_CONFIRMATION];
 	protected static $payableStatuses 		= [self::PENDING, self::AWAITS_PAYMENT];
 	protected static $billableStatuses 		= [self::ON_BILLING];
@@ -207,20 +211,22 @@ class OrderStatus extends Enum implements OrderStatusContract
 		}
 
 		static::$labels = [
-			self::IN_CREATION 				=> __('backoffice.order.in_creation'),
-			self::AWAITS_CONFIRMATION 		=> __('backoffice.order.awaits-confirmation'),
-			self::AWAITS_PAYMENT 			=> __('backoffice.order.awaits-payment'),
-			self::PENDING     				=> __('backoffice.order.pending'),
-			self::CANCELLED   				=> __('backoffice.order.cancelled'),
-			self::PAID        				=> __('backoffice.order.paid'),
-			self::IN_WAREHOUSE_PREPARATION 	=> __('backoffice.order.in-warehouse-preparation'),
-			self::PROCESSING 				=> __('backoffice.order.processing'),
-			self::ON_BILLING  				=> __('backoffice.order.on_billing'),
-			self::BILLED      				=> __('backoffice.order.billed'),
-			self::DISPATCHED  				=> __('backoffice.order.dispatched'),
-			self::REFUNDING 				=> __('backoffice.order.refunding'),
-			self::REFUNDED 					=> __('backoffice.order.refunded'),
-			self::COMPLETED   				=> __('backoffice.order.completed'),
+			self::IN_CREATION 						=> __('backoffice.order.in_creation'),
+			self::AWAITS_CONFIRMATION 				=> __('backoffice.order.awaits-confirmation'),
+			self::AWAITS_PAYMENT 					=> __('backoffice.order.awaits-payment'),
+			self::PENDING     						=> __('backoffice.order.pending'),
+			self::CANCELLED   						=> __('backoffice.order.cancelled'),
+			self::PAID        						=> __('backoffice.order.paid'),
+			self::IN_WAREHOUSE_PREPARATION 			=> __('backoffice.order.in-warehouse-preparation'),
+			self::IN_PREPARATION_PHARMACY_STORE 	=> __('backoffice.order.In preparation Pharmacy/Store'),
+			self::PROCESSING 						=> __('backoffice.order.processing'),
+			self::ON_BILLING  						=> __('backoffice.order.on_billing'),
+			self::BILLED      						=> __('backoffice.order.billed'),
+			self::DISPATCHED  						=> __('backoffice.order.dispatched'),
+			self::READY_FOR_PICKUP 					=> __('backoffice.order.Ready for pickup'),
+			self::REFUNDING 						=> __('backoffice.order.refunding'),
+			self::REFUNDED 							=> __('backoffice.order.refunded'),
+			self::COMPLETED   						=> __('backoffice.order.completed'),
 		];
 	}
 }
